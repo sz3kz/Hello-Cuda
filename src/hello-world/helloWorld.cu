2f98ca6fde2d10dc3e5b0ac366c5726ed4be8894
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-03-02 22:34:32 +0100

    Add block & thread identification to kernel invokation

--- src/hello-world/helloWorld.cu
+++ src/hello-world/helloWorld.cu
@@ -4,5 +4,7 @@ using namespace std;
 
 /* Declare kernel (function executable via GPU) */
 __global__ void helloWorld(){
-  printf("Hello World!\n");
+  int my_block_id = blockIdx.x;     // Identifies the specific invocation of the kernel
+  int my_thread_id = threadIdx.x;   // Identifies the specific thread running
+  printf("(%d|%d) Hello World!\n", my_block_id, my_thread_id);
 }
