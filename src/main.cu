5640032332f96ecd5927dc213bdef0c787c74011
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-03-02 09:42:40 +0100

    Refractor block and thread count as runtime variable

--- src/main.cu
+++ src/main.cu
@@ -3,9 +3,18 @@
 
 using namespace std;
 
-int main(){
+int main(int argc, char * argv[] ){
+  if (argc != 3){
+    puts("Invalid invocation format.");
+    printf("\tUsage: %s <blockAmount> <threadAmount>", argv[0]);
+    return 1;
+  }
+  long blocks = strtol(argv[1], NULL, 10);
+  long threads_per_block = strtol(argv[2], NULL, 10);
+  //printf("block: %ld \n",blocks);
+  //printf("threads per block: %ld \n",threads_per_block);
   cout << "Hello World!" << endl;
-  helloWorld<<<1,2>>>();
+  helloWorld<<<blocks, threads_per_block>>>();
   cudaDeviceSynchronize();
   return 0;
 }
